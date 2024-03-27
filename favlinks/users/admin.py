cfe2de747efd47993bb0aed874481c838c447f5a
JaceIntanai <jace.int.work@gmail.com>
2024-03-27 14:34:13 +0700

    Fix Q1-Q2

--- favlinks/users/admin.py
+++ favlinks/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+class userList(admin.ModelAdmin):
+    list_display = ("username","first_name","last_name","e_mail","create_dtm")
+
+admin.site.register(UserProfile, userList)
--- favlinks/users/models.py
+++ favlinks/users/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     e_mail = models.CharField(max_length=40)
-    date_created = models.DateTimeField(default=timezone.now)
+    create_dtm = models.DateTimeField(default=timezone.now)
 
     def __str__(self) :
-        return f"{self.username} : {self.first_name} {self.last_name} {self.e_mail} {self.date_created}"
+        return f"{self.username} : {self.first_name} {self.last_name} {self.e_mail} {self.create_dtm}"
